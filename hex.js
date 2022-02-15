f9422e9f09e9c79fe50d927c6b76bd36588b65e1
Your Name <you@example.com>
2022-02-15 21:34:21 +0530

    hex

--- hex.js
+++ hex.js
@@ -1,7 +1,7 @@
 const hex = [0,1,2,3,4,5,6,7,8,9,"A","B", "C", "D", "E", "F"]
 
 const btn = document.getElementById("action-btn");
-const color = document.getElementsByClassName("color");
+const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     let hexcolor = "#";
@@ -10,9 +10,10 @@ btn.addEventListener('click', function(){
         hexcolor += hex[getRandomNumber()];
     }
     color.textContent = hexcolor;
-    console.log(color.textContent)
+    console.log(color.textContent);
 
     document.body.style.backgroundColor = hexcolor;
+    color.textContent = hexcolor;
 });
 
 function getRandomNumber(){
